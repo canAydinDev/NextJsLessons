dd6f4c1f637a51df5724d6d73e81c55d2ef2f879
can aydin <canaydindev@gmail.com>
2024-02-20 02:06:40 +0300

    last last

--- src/auth.js
+++ src/auth.js
@@ -2,32 +2,32 @@
 
 
 
-import NextAuth from "next-auth/next";
-import Credentials from "next-auth/providers/credentials";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+// import NextAuth from "next-auth/next";
+// import Credentials from "next-auth/providers/credentials";
+// import GitHub from "next-auth/providers/github";
+// import Google from "next-auth/providers/google";
 
-const config = {
+// const config = {
   
-  providers: [GitHub, Google, Credentials({
-    credentials: {
-      username: { label: "Username"},
-      password: { label: "Password", type: "password"}
-    },
-    async authorize(credentials){ 
-      //buraya code'lar gelecek
-    }
-  })],
-  callbacks: {
-    //bu callback mutlaka tanimlanmali
-    //middleware icinde aktif hale getirilen route lara her giriste calisilir
-    //eger bu callback true donerse route icine girilir yoksa sign in sayfasina gidilir
-    authorized({request, auth}){
+//   providers: [GitHub, Google, Credentials({
+//     credentials: {
+//       username: { label: "Username"},
+//       password: { label: "Password", type: "password"}
+//     },
+//     async authorize(credentials){ 
+//       //buraya code'lar gelecek
+//     }
+//   })],
+//   callbacks: {
+//     //bu callback mutlaka tanimlanmali
+//     //middleware icinde aktif hale getirilen route lara her giriste calisilir
+//     //eger bu callback true donerse route icine girilir yoksa sign in sayfasina gidilir
+//     authorized({request, auth}){
       
-      return !!auth?.user
-    }
+//       return !!auth?.user
+//     }
     
-  }
-}
+//   }
+// }
 
-export const {handlers, auth} = NextAuth(config);
+// export const {handlers, auth} = NextAuth(config);
--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
-export { auth as middleware } from "./auth";
-//Bu satir authentication yapisinin tum uygulamada devreye alinmasini saglar.
+// export { auth as middleware } from "./auth";
+// //Bu satir authentication yapisinin tum uygulamada devreye alinmasini saglar.
 
-//eger istenirse config tanimlanarak bu kapsama alani belirlenebilir.
-export const config = {
-  //matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  matcher: [ '/login']
-};
+// //eger istenirse config tanimlanarak bu kapsama alani belirlenebilir.
+// export const config = {
+//   //matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+//   matcher: [ '/login']
+// };
